ea84165344c4d1914414a4e7c6dd66b44a9a1822
eLmincfrLu <elmincfrlu@gmail.com>
2025-11-30 15:22:51 +0000

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,13 +4,13 @@
  * main - Entry point
  *
  * Description: prints exactly
- * "Programming is like building a multilingual puzzle"
+ * "\"Programming is like building a multilingual puzzle"
  * followed by a new line, using puts function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
