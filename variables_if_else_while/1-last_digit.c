095d9fddad396ba240b0e5d5a34a190d3811e915
root <elmincfrlu@gmail.com>
2025-12-01 13:50:45 +0000

    hghgh

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,11 +19,14 @@ int main(void)
 	printf("Last digit of %d and is %d ",n,last_digit);
 
         if (last_digit>5){
-          printf("and is greater than 5"}
+          printf("and is greater than 5");
+ }
 	else if (last_digit==0){
-	  printf("and is 0") }
+	  printf("and is 0");
+ }
         else {
-	  printf("and is less than 6 and not 0") }
+	  printf("and is less than 6 and not 0");
+ }
 
 	return (0);
 }
