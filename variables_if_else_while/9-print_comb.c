d1204fe6df0cc7e7a5dac390673a04ebbc0fce4d
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-01 21:33:31 +0000

    eliko

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
+ * main - prints all single-digit numbers of base 10
  *
  * Return: 0
  */
 int main(void)
 {
-    int i, j;
+	int i;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-
-            if (!(i == 8 && j == 9))
-            {
-                putchar(', ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
