f631ca098a62157198650c551663070dfb1f54eb
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-01 20:13:44 +0000

    elikoduyaaa

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	char c;
-	
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-	   if (c != 'q' && c != 'e')
-	      putchar(c); 
+		if (c != 'q' && c != 'e')
+			putchar(c);
 	}
 	putchar('\n');
 	return (0);
