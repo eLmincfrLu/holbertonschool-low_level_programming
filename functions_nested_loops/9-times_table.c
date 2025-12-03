629c78adf381e3110edaa70e581337c1ee6211b9
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-03 18:03:30 +0000

    python

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,21 +7,35 @@
  */
 void times_table(void)
 {
-	int i;
-	int j;
-	int result;
+	int i, j, result;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			result = i * j;
-			_putchar(result + '0');
-			if (j != 9)
+
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
+
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
 			}
 		}
+		_putchar('\n');
 	}
 }
