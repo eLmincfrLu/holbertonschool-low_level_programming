e98eef350b55ae7512f332c0dcb47ac520d7d118
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-03 13:02:51 +0000

    python

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to process
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (n < 0)
+		last = -last;
+	_putchar(last + '0');
+}
