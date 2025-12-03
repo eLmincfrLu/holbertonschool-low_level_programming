7020ba16fff681d48563a6c262d9659f4e3a25fb
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-03 12:05:16 +0000

    python

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int a = 1;
+
+	while (a <= 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		a++;
+	}
+}
