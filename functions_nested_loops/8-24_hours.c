c326394647382f0862a099e02ed62e5e47ca902f
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-03 13:39:06 +0000

    python

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,11 +9,14 @@ void jack_bauer(void)
 
 	for (hour = 0; hour < 24;hour++)
 	{
-		_putchar(hour / 10 + '0');
-		_putchar(hour % 10 + '0');
-		_putchar(':');
-		_putchar(minute / 10 + '0');
-		_putchar(minute % 10 + '0');
-		_putchar('\n');
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+		}
 	}
 }
