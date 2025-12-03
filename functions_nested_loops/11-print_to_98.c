c4a58845eed982f851bf7d6ceabc8bf6d30e3e34
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-03 18:37:50 +0000

    python

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,15 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for(; n <= 98; n++)
+		for(; n < 98; n++)
 		{
-			printf("%d "; n);
+			printf("%d, "; n);
 		}
 	}
 	else
 	{
-		for (; n >= 98; n--);
-			printf("%d "; n);
+		for (; n > 98; n--);
+			printf("%d, "; n);
 	}
 	printf("98\n";
 }
