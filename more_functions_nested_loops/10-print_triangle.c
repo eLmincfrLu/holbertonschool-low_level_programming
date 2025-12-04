a700b7391e16c541115c7569c22a12b3fc4ce534
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-04 10:00:58 +0000

    python

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle using #
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int a, b;
+	int ct;
+	int cy;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= 10; a++)
+		{
+			for (ct = 1; ct <= 10 - a; ct++)
+			{
+				_putchar(' ');
+			}
+			for (b = 1; b <=  a; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
