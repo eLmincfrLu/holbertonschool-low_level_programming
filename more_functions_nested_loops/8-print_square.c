26e55a3bd7b257c9ede54f2c379b11483b7cc973
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-04 09:15:55 +0000

    python

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the # character
+ * @n: size of the square
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size >= 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
