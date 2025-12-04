3c7eb5a4285f441016872f828d4d60cefa780a5a
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-04 08:47:25 +0000

    python

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int j = 0;
+
+	if (n != 0)
+	{
+		while (j <= n)
+		{
+			_putchar('_');
+			j++;
+		}
+	}
+	_putchar('\n');
+}
