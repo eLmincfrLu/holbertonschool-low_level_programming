1d626087ba07a0e8f5c161e3cf8c2fbbaba2e138
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-14 09:45:25 +0000

    python

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -8,33 +9,24 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	int sign;
-	int result;
-	int started;
-
-	i = 0;
-	sign = 1;
-	result = 0;
-	started = 0;
+	int i = 0, sign = 1, result = 0, started = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
+			if (sign == 1 && result > (INT_MAX - (s[i] - '0')) / 10)
+				return (INT_MAX);
+			if (sign == -1 && -result < (INT_MIN + (s[i] - '0')) / 10)
+				return (INT_MIN);
 			result = result * 10 + (s[i] - '0');
 		}
 		else if (started)
-		{
 			break;
-		}
-
 		i++;
 	}
-
 	return (result * sign);
 }
