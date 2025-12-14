09f97c5578b43ef1393eb6d01b214c4e63368ed1
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-14 09:49:02 +0000

    python

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,17 +11,17 @@ int _atoi(char *s)
 {
 	int i = 0, sign = 1, result = 0, started = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] == '-')
 			sign *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
-			if (sign == 1 && result > (INT_MAX - (s[i] - '0')) / 10)
-				return (INT_MAX);
-			if (sign == -1 && -result < (INT_MIN + (s[i] - '0')) / 10)
-				return (INT_MIN);
+			/* check overflow before adding digit */
+			if (result > (INT_MAX - (s[i] - '0')) / 10)
+				return (sign == 1 ? INT_MAX : INT_MIN);
 			result = result * 10 + (s[i] - '0');
 		}
 		else if (started)
