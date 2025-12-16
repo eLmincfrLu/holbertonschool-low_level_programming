5daa95a949579f1b5808ce9a01ebb0ab5ca4f415
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-16 07:30:57 +0000

    python

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,13 +7,13 @@
  *
  * Return: natural square root, or -1 if not found
  */
-int sqrt_help(int n, int guess)
+int _sqrt_helper(int n, int guess)
 {
 	if ((guess * guess) == n)
 		return (guess);
 	if ((guess * guess) > n)
 		return (-1);
-	return (sqrt_help(n, guess + 1));
+	return (_sqrt_helper(n, guess + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
