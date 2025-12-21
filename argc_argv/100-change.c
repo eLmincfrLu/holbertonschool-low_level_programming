6f70d5b20ac23f1f4ffd7c94c00d05819287a1d2
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-21 18:53:53 +0000

    python

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 
 /**
+ * get_coins - calculates minimum number of coins
+ * @cents: amount in cents
+ *
+ * Return: number of coins
+ */
+int get_coins(int cents)
+{
+	int count = 0;
+
+	while (cents >= 25)
+	{
+		cents -= 25;
+		count++;
+	}
+	while (cents >= 10)
+	{
+		cents -= 10;
+		count++;
+	}
+	while (cents >= 5)
+	{
+		cents -= 5;
+		count++;
+	}
+	while (cents >= 2)
+	{
+		cents -= 2;
+		count++;
+	}
+	while (cents >= 1)
+	{
+		cents -= 1;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * main - prints the minimum number of coins to make change
  * @argc: argument count
  * @argv: argument vector
@@ -11,7 +50,6 @@
 int main(int argc, char *argv[])
 {
 	int cents;
-	int count = 0;
 
 	if (argc != 2)
 	{
@@ -27,37 +65,7 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	while (cents >= 25)
-	{
-		cents -= 25;
-		count++;
-	}
-
-	while (cents >= 10)
-	{
-		cents -= 10;
-		count++;
-	}
-
-	while (cents >= 5)
-	{
-		cents -= 5;
-		count++;
-	}
-
-	while (cents >= 2)
-	{
-		cents -= 2;
-		count++;
-	}
-
-	while (cents >= 1)
-	{
-		cents -= 1;
-		count++;
-	}
-
-	printf("%d\n", count);
+	printf("%d\n", get_coins(cents));
 
 	return (0);
 }
