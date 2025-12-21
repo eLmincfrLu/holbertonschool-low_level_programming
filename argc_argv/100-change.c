eef89109bd8cd0776d3303d03b82e405096030b3
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-21 18:52:46 +0000

    python

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -11,10 +11,9 @@
 int main(int argc, char *argv[])
 {
 	int cents;
-	int coins = 0;
-	int a;
+	int count = 0;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -24,46 +23,41 @@ int main(int argc, char *argv[])
 
 	if (cents < 0)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
 		return (0);
 	}
 
-	if (cents >= 25)
+	while (cents >= 25)
 	{
-		a = cents / 25;
-		cents = cents - (a * 25);
-		coins += a;
+		cents -= 25;
+		count++;
 	}
 
-	if (cents >= 10)
+	while (cents >= 10)
 	{
-		a = cents / 10;
-		cents = cents - (a * 10);
-		coins += a;
+		cents -= 10;
+		count++;
 	}
 
-	if (cents >= 5)
+	while (cents >= 5)
 	{
-		a = cents / 5;
-		cents = cents - (a * 5);
-		coins += a;
+		cents -= 5;
+		count++;
 	}
 
-	if (cents >= 2)
+	while (cents >= 2)
 	{
-		a = cents / 2;
-		cents = cents - (a * 2);
-		coins += a;
+		cents -= 2;
+		count++;
 	}
 
-	if (cents >= 1)
+	while (cents >= 1)
 	{
-		a = cents / 1;
-		cents = cents - (a * 1);
-		coins += a;
+		cents -= 1;
+		count++;
 	}
 
-	printf("%d\n", coins);
+	printf("%d\n", count);
 
 	return (0);
 }
