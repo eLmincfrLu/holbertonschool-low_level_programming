17117f36844d4fc390ca3b2133272122c101b6cf
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-27 21:09:56 +0000

    python

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size in bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ *         if malloc fails, terminates process with status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+
+    ptr = malloc(b);
+    if (ptr == NULL)
+        exit(98);  // terminate with status 98 if allocation fails
+
+    return (ptr);
+}
