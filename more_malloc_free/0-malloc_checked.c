2c35458a20d3dc299b250beb12f715192b0b2571
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-27 21:11:40 +0000

    python

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -14,7 +14,7 @@ void *malloc_checked(unsigned int b)
 
     ptr = malloc(b);
     if (ptr == NULL)
-        exit(98);  // terminate with status 98 if allocation fails
+        exit(98);
 
     return (ptr);
 }
