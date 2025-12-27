7bca35f3532ed8625046581188b1f5f403ee381c
eLmincfrLu <elmincfrlu@gmail.com>
2025-12-27 21:00:04 +0000

    python

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *           which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string, or NULL if str is NULL
+ *         or if memory allocation fails
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	dup = malloc((len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[i] = '\0';
+
+	return (dup);
+}
