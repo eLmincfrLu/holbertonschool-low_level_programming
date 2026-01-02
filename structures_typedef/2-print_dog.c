cfdcf25effbb9c07a230a4d964ab3493fabbfb6c
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-02 16:36:08 +0000

    python

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,20 +1,16 @@
-#include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * print_dog - prints a struct dog
  * @d: pointer to the dog to print
- *
- * Description: if a member is NULL, prints (nil) instead
- *              if d is NULL, prints nothing
  */
-void print_dog(dog_t *d);
+void print_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
+if (d == NULL)
+return;
 
-	printf("Name: %s\n", d->name != NULL ? d->name : "(nill)");
-	printf("Age: %d\n", d->age);
-	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nill)");
+printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
