715e6216929113ed934b21042be93cccb07c2225
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-02 17:42:34 +0000

    python

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,13 +7,13 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    if (d->name != NULL)
-        free(d->name);
-    if (d->owner != NULL)
-        free(d->owner);
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
 
-    free(d);
+	free(d);
 }
