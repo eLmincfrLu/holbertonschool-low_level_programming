a135aa1f8d506d666f24a90643be27d92e530bb9
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-02 17:40:24 +0000

    python

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+        return;
+
+    if (d->name != NULL)
+        free(d->name);
+    if (d->owner != NULL)
+        free(d->owner);
+
+    free(d);
+}
