db3dde4fb52bdd630eb1f262269a4789937236ad
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-04 08:23:57 +0000

    python

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: pointer to comparison function
+ *
+ * Return: index of first element for which cmp != 0, else -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
