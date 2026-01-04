fb60609790abf79364a7e6627f8236848f48d6f9
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-04 08:14:31 +0000

    python

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,9 +9,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array == NULL || action == NULL)
-        return;
+	size_t i;
+	if (array == NULL || action == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-        action(array[i]);
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
