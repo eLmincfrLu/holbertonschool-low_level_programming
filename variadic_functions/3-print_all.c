eafc9d6a7321ca54511c58b7f7c7a3c09a2b975b
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-09 21:24:35 +0000

    python

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,56 +1,48 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_all - Verilmiş format əsasında istənilən tipdə dəyərləri çap edir
- * @format: format string: c = char, i = int, f = float, s = string
- *
- * Description: Əgər string NULL-dursa, (nil) çap edir.
- * Hər dəyər arasına ", " qoyur.
- * Yalnız 2 while, 2 if və maksimum 9 dəyişən istifadə olunur.
+ * print_all - prints anything based on a format string
+ * @format: list of types of arguments
+ * c: char
+ * i: integer
+ * f: float
+ * s: string
+ * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *sep = "";
 	char *str;
+	char sep;
+	unsigned int i;
 
 	va_start(args, format);
-
-	if (format != NULL)  /* 1-ci if */
+	i = 0;
+	sep = 0;
+	while (format && format[i])
 	{
-		while (format[i] != '\0')  /* 1-ci while */
+		if (sep)
+			printf(", ");
+		if (format[i] == 'c')
+			printf("%c", va_arg(args, int));
+		if (format[i] == 'i')
+			printf("%d", va_arg(args, int));
+		if (format[i] == 'f')
+			printf("%f", va_arg(args, double));
+		if (format[i] == 's')
 		{
-			if (format[i] == 'c' || format[i] == 'i' ||
-			    format[i] == 'f' || format[i] == 's')  /* 2-ci if */
-			{
-				/* char */
-				if (format[i] == 'c')
-					printf("%s%c", sep, va_arg(args, int));
-
-				/* int */
-				if (format[i] == 'i')
-					printf("%s%d", sep, va_arg(args, int));
-
-				/* float */
-				if (format[i] == 'f')
-					printf("%s%f", sep, va_arg(args, double));
-
-				/* string */
-				if (format[i] == 's')
-				{
-					str = va_arg(args, char *);
-					printf("%s%s", sep, str ? str : "(nil)");
-				}
-
-				sep = ", ";
-			}
-			i++;
+			str = va_arg(args, char *);
+			if (!str)
+				printf("(nil)");
+			else
+				printf("%s", str);
 		}
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+			sep = 1;
+		i++;
 	}
-
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
