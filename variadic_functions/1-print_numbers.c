8d9fcf0cf954c16c5e7b4622a46296b8863f069d
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-09 20:50:48 +0000

    python

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Verilmiş ədədləri çap edir
+ * @separator: ədədlər arasına qoyulacaq string
+ * @n: Pass edilmiş ədədlərin sayı
+ *
+ * Description: Əgər separator NULL-dursa, onu çap etmir.
+ * Yeni sətr ilə bitir.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (seperator != NULL && i < n - 1)
+			printf("%s", seperator);
+	}
+	va_end(args);
+
+	printf("\n");
+}
