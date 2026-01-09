ecdc3419332aaf6ab5de8906cc7058efc65692d1
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-09 20:44:02 +0000

    python

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -10,5 +10,6 @@
  * Return: Parametrlərin cəmi, əgər n == 0 olarsa 0
  */
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
