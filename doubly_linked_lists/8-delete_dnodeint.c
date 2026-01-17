41c83850480e09959254ec24c685b65186b7f67c
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-17 22:15:25 +0000

    python

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,37 +10,36 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int i;
+	dlistint_t *current;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    current = *head;
+	current = *head;
 
-    /* delete head node */
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
+	/* delete head node */
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
 
-    /* move to the node at index */
-    for (i = 0; current != NULL && i < index; i++)
-        current = current->next;
+	/* move to the node at index */
+	for (i = 0; current != NULL && i < index; i++)
+		current = current->next;
 
-    if (current == NULL)
-        return (-1);
+	if (current == NULL)
+		return (-1);
 
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
 
-    free(current);
-    return (1);
+	free(current);
+	return (1);
 }
-
