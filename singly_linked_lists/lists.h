3fa9e300dac83724978f3f5b64263a4f45e79a16
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-17 20:33:55 +0000

    python

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,9 +16,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/* Prototypes of all your functions */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);  /* <- BU SƏTİR ƏLAVƏ ETMƏLİSƏN */
 
 #endif
