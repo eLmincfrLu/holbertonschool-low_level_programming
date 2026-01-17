7612f09f0302d3b6009bc1011fe608130fa38081
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-17 22:14:01 +0000

    python

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index
+ * @head: pointer to pointer to the head of the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 if success, -1 if failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current = *head;
+
+    /* delete head node */
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+
+    /* move to the node at index */
+    for (i = 0; current != NULL && i < index; i++)
+        current = current->next;
+
+    if (current == NULL)
+        return (-1);
+
+    if (current->prev != NULL)
+        current->prev->next = current->next;
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    free(current);
+    return (1);
+}
+
