5fa06dd0d7876c4480af7b8bdd5440b7e4d0628e
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-17 21:55:14 +0000

    python

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer stored in the node
+ * @prev: pointer to the previous element
+ * @next: pointer to the next element
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes */
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+
+#endif /* LISTS_H */
