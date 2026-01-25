3cea43cc53cbd770b8f28e987cec68cda9fd4b6d
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-25 08:12:32 +0000

    python

---
+++ file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+
+/**
+ * read_textfile - reads a text file and prints it to STDOUT
+ * @filename: name of the file
+ * @letters: number of letters to read and print
+ *
+ * Return: actual number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
