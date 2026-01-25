34d82522430c823b2ead4f6e1ea9117d352f1272
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-25 08:13:49 +0000

    python

--- file_io/main.h
+++ file_io/main.h
@@ -11,5 +11,6 @@
  * Return: actual number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
