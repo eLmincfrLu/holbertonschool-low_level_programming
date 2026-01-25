a4d2a630844af9596fd310b7d2b9134bbfb1977c
eLmincfrLu <elmincfrlu@gmail.com>
2026-01-25 08:17:13 +0000

    python

---
+++ file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * close_fd - closes a file descriptor
+ * @fd: file descriptor to close
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+	ssize_t rd, wr;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		close_fd(fd_from);
+		exit(99);
+	}
+
+	while ((rd = read(fd_from, buffer, 1024)) > 0)
+	{
+		wr = write(fd_to, buffer, rd);
+		if (wr == -1 || wr != rd)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (rd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
